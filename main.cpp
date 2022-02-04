3b287422faed45833c9039bf8cb61a16d650bfb7
Marcel Nowicki <github@epenis.de>
2022-02-04 19:00:29 +0100

    Erste Versuche, Problem: tar öffnet nicht

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    if (std::ifstream datei{"test.tar", std::ios::binary}) {
+        int buffersize = 512;
+        char *buffer;
+
+        datei.read(buffer, buffersize);
+
+
+        datei.close();
+
+        std::cout << buffer[0] << buffer[1] << std::endl;
+    }
+
+    else
+        std::cout << " Fehler ! " << std::endl;
     return 0;
 }
